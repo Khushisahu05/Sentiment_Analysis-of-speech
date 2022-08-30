e48bb7c490ed7b4a7182541661030574f20db725
Khushisahu05 <92144074+Khushisahu05@users.noreply.github.com>
2022-08-31 01:41:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import string
+import matplotlib.pyplot as plt
+from collections import Counter
+text = open('Read.txt',encoding='utf-8').read()
+lower_case = text.lower()
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+tokenized_words = cleaned_text.split()
+
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+# remove stopwords
+final_words =[]
+for words in tokenized_words:
+    if words not in stop_words:
+        final_words.append(words)
+# get emotion text
+emotion_list =[]
+with open('emotions.txt','r') as file:
+    for line in file:
+        clear_line = line.replace("\n",'').replace(',','').replace("'",'').strip()
+        word,emotions = clear_line.split(':')
+
+        if word in final_words:
+            emotion_list.append(emotions)
+
+print(emotion_list)
+w = Counter(emotion_list)
+print(w)
+
+fig,axl = plt.subplots()
+axl.bar(w.keys(),w.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
